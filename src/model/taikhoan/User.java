8621dcb9760084811893b5ad6b5eda2a8a322a35
dnh_an <dinhan.0321@gmail.com>
2025-08-07 00:55:42 +0700

    fix losing msv bug

--- src/model/taikhoan/User.java
+++ src/model/taikhoan/User.java
@@ -1,14 +1,28 @@
 package model.taikhoan;
 
 public class User extends TaiKhoan {
+    private String msv;
     public User() {}
 
-    public User(String tenDangNhap, String matKhau) {
+    public User(String tenDangNhap, String matKhau, String msv) {
+
         super(tenDangNhap, matKhau);
+        this.msv = msv;
     }
 
+    public String getMsv() {
+        return msv;
+    }
+    public void setMsv(String msv) {
+        this.msv = msv;
+    }
     @Override
     public String getRole() {
         return "USER";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | MSSV: " + msv;
     }
 }
