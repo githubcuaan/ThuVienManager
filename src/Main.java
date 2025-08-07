52ac61d1ea3abd93c8c583885f6d563412a1d9e7
dnh_an <dinhan.0321@gmail.com>
2025-08-07 09:57:17 +0700

    add UTF-8 encoding

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 import ui.MenuChinh;
 
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        try {
+            System.setOut(new PrintStream(System.out, true, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            System.err.println("Không hỗ trợ UTF-8 bro 😭");
+        }
         new MenuChinh().start();
     }
 }
